aeb19edd75044b710f5fc46d825f8d6f8d8454ae
Daisuke NASHIRO <nashirox@gmail.com>
2014-06-26 20:49:43 +0900

    現時点での最新版をプッシュ

--- confirm.php
+++ confirm.php
@@ -175,6 +175,16 @@ EOM;
     <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
     <script src="js/classie.js"></script>
     <script src="js/freelancer.js"></script>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  
+    ga('create', 'UA-51728793-1', 'wmegane.com');
+    ga('send', 'pageview');
+    ga(‘set’, ‘&uid’, {{USER_ID}}); // ログインしている user_id を使用してUser-ID を設定します。  
+  </script>
 
 </body>
 
--- sformmail.php
+++ sformmail.php
@@ -141,6 +141,17 @@ function Err($err) { // エラー表示用
 <body style="font-size: 12px; line-height: 1.8em;">
 <strong>エラー : </strong>$err<br>
 <input type="button" value="戻る" onclick="history.back();">
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  
+    ga('create', 'UA-51728793-1', 'wmegane.com');
+    ga('send', 'pageview');
+    ga(‘set’, ‘&uid’, {{USER_ID}}); // ログインしている user_id を使用してUser-ID を設定します。  
+  </script>
+
 </body></html>
 EOM;
 	exit;
